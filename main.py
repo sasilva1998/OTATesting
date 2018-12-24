cfa168b2f2beb8218843f7091a75dbef6d172417
sasilva1998 <30530787+sasilva1998@users.noreply.github.com>
2018-12-24 07:10:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-for i in range(0,10):
-  print(i)
+from ota_updater import OTAUpdater
+
+
+def download_and_install_update_if_available():
+    ota_updater = OTAUpdater('https://github.com/sasilva1998/OTATesting')
+    ota_updater.download_and_install_update_if_available('FLIA SILVA','generacionsilva1976')
+
+def start():
+  for i in range(0,5):
+    print(i)
+
+def boot():
+    download_and_install_update_if_available()
+    start()
+    
+boot()
