c26160cc2b55aee2fa9ca5d767b95ac587966626
Steven <sasilva1998@gmail.com>
2019-11-01 12:04:49 -0500

    t

--- main/main.py
+++ main/main.py
@@ -3,8 +3,8 @@ from main.ota_updater import OTAUpdater
 
 def download_and_install_update_if_available():
    
-   o = OTAUpdater('https://github.com/sasilva1998/OTATesting.git',main_dir='/')
-   o._download_and_install_update('NETLIFE-Silva', 'SASM3141')
+   o = OTAUpdater('https://github.com/sasilva1998/OTATesting.git')
+   o.download_and_install_update_if_available('NETLIFE-Silva', 'SASM3141')
 
 
 def start():
