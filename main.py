31da4e675b7befe9e5737d5ccbf87a22f6f768f1
Steven <sasilva1998@gmail.com>
2019-11-01 12:31:22 -0500

    name

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def download_and_install_update_if_available():
 def start():
 	from main.ota_updater
 	from main.action import action
-	
+
 	action()
 
 	ota_updater.check_for_update_to_install_during_next_reboot()
