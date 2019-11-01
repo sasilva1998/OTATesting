edbe7d4062fe334b7032019c1b16bc6a06f40c02
Steven <sasilva1998@gmail.com>
2019-11-01 10:21:58 -0500

    typos

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from ota_update.main.ota_updater import OTAUpdater
+from main.ota_updater import OTAUpdater
 
 
 def download_and_install_update_if_available():
    
-   o = OTAUpdater('url-to-your-github-project')
-   o.download_and_install_update_if_available('wifi-ssid', 'wifi-password')
+   o = OTAUpdater('https://github.com/sasilva1998/OTATesting.git')
+   o.download_and_install_update_if_available('NETLIFE-Silva', 'SASM3141')
 
 
 def start():
-	for i in range(0,5):
+	for i in range(0,10):
 		print(i*2)
 
 
